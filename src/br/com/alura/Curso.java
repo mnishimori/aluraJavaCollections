9322128c0fb057d20e791ceb8936a0380d7ae128
mnishimori <mnishimori@yahoo.com.br>
2022-08-18 20:35:02 -0300

    Set collections

--- src/br/com/alura/Curso.java
+++ src/br/com/alura/Curso.java
@@ -2,13 +2,16 @@ package br.com.alura;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Curso {
 	
 	private String nome;
 	private String instrutor;
 	private List<Aula> aulas = new ArrayList<>();
+	private Set<Aluno> alunos = new HashSet<>();
 	
 	
 	public Curso(String nome, String instrutor) {
@@ -19,9 +22,11 @@ public class Curso {
 	public String getNome() {
 		return nome;
 	}
+	
 	public String getInstrutor() {
 		return instrutor;
 	}
+	
 	public List<Aula> getAulas() {
 		return Collections.unmodifiableList(this.aulas);
 	}
@@ -32,6 +37,14 @@ public class Curso {
 	
 	public Integer getTempoTotal() {
 		return this.aulas.stream().mapToInt(Aula::getTempo).sum();
+	}
+	
+	public void matricula(Aluno aluno) {
+		this.alunos.add(aluno);
+	}
+	
+	public Set<Aluno> getAlunos(){
+		return Collections.unmodifiableSet(this.alunos);
 	}
 	
 	 @Override
