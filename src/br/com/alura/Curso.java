91f2b40ced68efdc38eab81ec8daf91ad7e3e7cc
mnishimori <mnishimori@yahoo.com.br>
2022-08-19 19:52:11 -0300

    Equals e Hashcode

--- src/br/com/alura/Curso.java
+++ src/br/com/alura/Curso.java
@@ -7,49 +7,82 @@ import java.util.List;
 import java.util.Set;
 
 public class Curso {
-	
+
 	private String nome;
 	private String instrutor;
 	private List<Aula> aulas = new ArrayList<>();
 	private Set<Aluno> alunos = new HashSet<>();
-	
-	
+
 	public Curso(String nome, String instrutor) {
+		if (nome == null) {
+			throw new NullPointerException("O nome do aluno está nulo");
+		}
 		this.nome = nome;
 		this.instrutor = instrutor;
 	}
-	
+
 	public String getNome() {
 		return nome;
 	}
-	
+
 	public String getInstrutor() {
 		return instrutor;
 	}
-	
+
 	public List<Aula> getAulas() {
 		return Collections.unmodifiableList(this.aulas);
 	}
-	
+
 	public void adiciona(Aula aula) {
 		this.aulas.add(aula);
 	}
-	
+
 	public Integer getTempoTotal() {
 		return this.aulas.stream().mapToInt(Aula::getTempo).sum();
 	}
-	
+
 	public void matricula(Aluno aluno) {
 		this.alunos.add(aluno);
 	}
-	
-	public Set<Aluno> getAlunos(){
+
+	public Set<Aluno> getAlunos() {
 		return Collections.unmodifiableSet(this.alunos);
 	}
+
+	public Boolean estaMatriculado(Aluno aluno) {
+		return this.getAlunos().contains(aluno);
+	}
+
+	@Override
+	public String toString() {
+		return "[Curso: " + this.getNome() + ", tempo total: " + this.getTempoTotal() + ", aulas: + " + this.aulas
+				+ "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Curso other = (Curso) obj;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
 	
-	 @Override
-    public String toString() {
-        return "[Curso: " + this.getNome() + ", tempo total: " + this.getTempoTotal()
-                + ", aulas: + " + this.aulas + "]";
-    }
+	
 }
